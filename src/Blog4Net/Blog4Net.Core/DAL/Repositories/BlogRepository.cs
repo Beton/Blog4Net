08cd4b587181abbe3e18387b0cfe010f8d98fc38
Aleksander Bethke <alexander.bethke@gmail.com>
2013-03-26 10:21:22 +0100

    Added blog repository

---
+++ src/Blog4Net/Blog4Net.Core/DAL/Repositories/BlogRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Blog4Net.Core.Domain;
+using NHibernate;
+
+namespace Blog4Net.Core.DAL.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly ISession session;
+
+        public BlogRepository(ISession session)
+        {
+            this.session = session;
+        }
+
+        public IList<Post> Posts(int pageNumber, int pageSize)
+        {
+            var query = session.QueryOver<Post>()
+                               .Where(post => post.Published)
+                               .OrderBy(post => post.PostedOn).Desc
+                               .Fetch(post => post.Category).Eager
+                               .Fetch(post => post.Tags).Eager
+                               .Skip(pageNumber*pageSize)
+                               .Take(pageSize)
+                               .Future();
+
+            return query.ToList();
+        }
+
+        public int TotalPosts()
+        {
+            var query = session.QueryOver<Post>().Where(post => post.Published).RowCount();
+
+            return query;
+        }
+    }
+}
---
+++ src/Blog4Net/Blog4Net.Core/DAL/Repositories/IBlogRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Blog4Net.Core.Domain;
+
+namespace Blog4Net.Core.DAL.Repositories
+{
+    public interface IBlogRepository
+    {
+        IList<Post> Posts(int pageNumber, int pageSize);
+        int TotalPosts();
+    }
+}
