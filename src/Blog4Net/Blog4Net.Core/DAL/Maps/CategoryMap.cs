e75326c0c0cdc90c2f2e51d76253ee65ce0c70ee
Aleksander Bethke <alexander.bethke@gmail.com>
2013-03-26 09:38:38 +0100

    Added NHibernate domain maps

---
+++ src/Blog4Net/Blog4Net.Core/DAL/Maps/CategoryMap.cs
@@ -0,0 +1,29 @@
+﻿using Blog4Net.Core.Domain;
+using FluentNHibernate.Mapping;
+
+namespace Blog4Net.Core.DAL.Maps
+{
+    public class CategoryMap : ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Name)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.UrlSlug)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.Description)
+                .Length(200);
+
+            HasMany(x => x.Posts)
+                .Inverse()
+                .Cascade.All()
+                .KeyColumn("CategoryId");
+        }         
+    }
+}
---
+++ src/Blog4Net/Blog4Net.Core/DAL/Maps/TagMap.cs
@@ -0,0 +1,28 @@
+﻿using Blog4Net.Core.Domain;
+using FluentNHibernate.Mapping;
+
+namespace Blog4Net.Core.DAL.Maps
+{
+    public class TagMap : ClassMap<Tag>
+    {
+        public TagMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Name)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.UrlSlug)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.Description)
+                .Length(200);
+
+            HasManyToMany(x => x.Posts)
+                .Cascade.All().Inverse()
+                .Table("PostTagMap");
+        }
+    }
+}
