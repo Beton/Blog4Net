e36a9b3c50555ea50ad4cccf92a9b0dfd3a8b184
Aleksander Bethke <alexander.bethke@gmail.com>
2013-03-26 18:27:00 +0100

    Posts action implemented

---
+++ src/Blog4Net/Blog4Net.Web/Utility/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace Blog4Net.Web.Utility
+{
+    public static class Extensions
+    {
+        public static string ToConfigLocalTime(this DateTime utcDt)
+        {
+            var istTz = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
+            return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDt, istTz).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
+        } 
+    }
+}
