4b52ddba43feeb3cee35be878733cd1b353087be
Aleksander Bethke <alexander.bethke@gmail.com>
2013-03-29 18:05:24 +0100

    Ninject Kernel refactoring

--- src/Blog4Net/Blog4Net.Web/Global.asax.cs
+++ src/Blog4Net/Blog4Net.Web/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 using Blog4Net.Core.DAL.Repositories;
 using Blog4Net.Core.Infrastructure.IoC.Modules;
@@ -9,14 +11,25 @@ using Ninject.Web.Common;
 namespace Blog4Net.Web
 {    
     public class MvcApplication : NinjectHttpApplication
-    {        
+    {
+        private static IKernel kernel;
+        private static readonly object Lock = new object();
+
+        public new IKernel Kernel
+        {
+            get { return kernel; }
+        }
+
         protected override IKernel CreateKernel()
         {
-            var kernel = new StandardKernel();
-            
-            kernel.Load(new RepositoryModule());
-            kernel.Bind<IBlogRepository>().To<BlogRepository>();
+            lock (Lock)
+            {
+                kernel = new StandardKernel();
 
+                kernel.Load(new RepositoryModule());
+                kernel.Bind<IBlogRepository>().To<BlogRepository>();
+            }
+                                    
             return kernel;
         }
 
@@ -27,6 +40,20 @@ namespace Blog4Net.Web
             
             base.OnApplicationStarted();
         }
-        
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError();
+            var httpError = ex as HttpException;
+
+            if (httpError != null && httpError.GetHttpCode() == 404) return;
+
+            Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
+        }
+
+        protected void OnApplicationEnded()
+        {            
+            kernel.Dispose();
+        }
     }
 }
